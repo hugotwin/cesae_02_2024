85928e6fe2421cca88a547b5e05fcbe09b379d0f
Hugo Maia <130175150+hugotwin@users.noreply.github.com>
2024-02-19 12:59:32 +0000

    inicio

---
+++ ALG_HUGO_MAIA/Ficha_java/src/Ficha_01/Exercicio04.java
@@ -0,0 +1,20 @@
+package Ficha_01;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        // criar variaveis
+
+        double raio, pi=3.1415, area;
+        // ler as variaveis
+        Scanner input_ = new Scanner(System.in);
+        System.out.println("insira o valor do raio :");
+        raio = input_.nextInt();
+        area = raio*raio*pi;
+
+        System.out.println("A sua área : " + area);
+    }
+}
+
+
---
+++ ALG_HUGO_MAIA/Ficha_java/src/Ficha_01/Exercicio07.java
@@ -0,0 +1,27 @@
+package Ficha_01;
+
+import java.util.Scanner;
+public class Exercicio07 {
+    public static void main(String[] args) {
+
+        // criar varivaeis
+        double valor1, valor2, valor3, valorFinal;
+        // ler variaveis
+        
+        Scanner input_ = new Scanner(System.in);
+        System.out.println("insiara  o primeiro valor");
+        valor1 = input_.nextDouble();
+        System.out.println("insiara  o valor2");
+        valor2 = input_.nextDouble();
+        System.out.println("insiara  o valor3");
+        valor3 = input_.nextDouble();
+
+        valorFinal = ((valor1+valor2+valor3)/1.10);
+
+
+
+
+        System.out.println("insiara  o primeiro valor");
+
+    }
+}
