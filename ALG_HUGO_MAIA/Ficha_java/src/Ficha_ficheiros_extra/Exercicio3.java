92363832e5463c6c85137b074c3da83cd2b825af
Hugo Maia <130175150+hugotwin@users.noreply.github.com>
2024-03-20 17:00:01 +0000

    20/03/2023_

--- ALG_HUGO_MAIA/Ficha_java/src/Ficha_ficheiros_extra/Exercicio3.java
+++ ALG_HUGO_MAIA/Ficha_java/src/Ficha_ficheiros_extra/Exercicio3.java
@@ -2,84 +2,67 @@ package Ficha_ficheiros_extra;
 
 import java.util.Scanner;
 
-import static Ficha_ficheiros_extra.Exercicio3_funcoes.gerir_bases_dados;
-import static Ficha_ficheiros_extra.Exercicio3_funcoes.verificar_acesso;
+import static Ficha_ficheiros_extra.Exercicio3_funcoes.*;
 
 public class Exercicio3 {
 
 
     public static void main(String[] args) {
 
-
-
-
-
         try {
 
 
+            if (verificar_acesso("C:/Users/softdev/Desktop/cesae_02_2024/ALG_HUGO_MAIA/Ficha_java/src/Ficha_ficheiros_extra/Ex_03 Hotel Temático/loginHotel.txt")) {
+                System.out.println("indique a opção : \n1 - Gerir Base de Dados" +
+                        "\n2 - Consultar Clientes" +
+                        "\n3 - Consultar Quartos Disponíveis " +
+                        "\n4 - Consultar Reservas" +
+                        "\n5 - Registar Nova Reserva" +
+                        "\n6 - Dashboard de Análise" +
+                        "\n7 - Galeria ");
+                Scanner input = new Scanner(System.in);
+                String opcao = input.nextLine().trim().toLowerCase();
+                switch (opcao) {
+                    case "1":
 
-           if(verificar_acesso("C:/Users/softdev/Desktop/cesae_02_2024/ALG_HUGO_MAIA/Ficha_java/src/Ficha_ficheiros_extra/Ex_03 Hotel Temático/loginHotel.txt"))
-           {
-               System.out.println("indique a opção : \n1 - Gerir Base de Dados" +
-                       "\n2 - Consultar Clientes" +
-                       "\n3 - Consultar Quartos Disponíveis " +
-                       "\n4 - Consultar Reservas" +
-                       "\n5 - Registar Nova Reserva" +
-                       "\n6 - Dashboard de Análise" +
-                       "\n7 - Galeria ");
-               Scanner input = new Scanner(System.in);
-               String opcao = input.nextLine().trim().toLowerCase();
-               switch (opcao){
-                   case "1" :
+                        gerir_bases_dados();
 
-                       gerir_bases_dados();
+                        break;
+                    case "2":
+                        System.out.println(consultarclientes());
+                        break;
 
-                       break;
-                   case "2":
+                    case "3":
 
-                       break;
+                        break;
+                    case "4":
 
-                   case "3":
+                        break;
 
-                       break;
-                   case "4":
+                    case "5":
 
-                       break;
+                        break;
 
-                   case "5":
+                    case "6":
 
-                       break;
+                        break;
 
-                   case "6":
+                    case "7":
 
-                       break;
+                        break;
 
-                   case "7":
+                    default:
 
-                       break;
-
-                   default :
-
-                       break;
+                        break;
 
 
+                }
 
 
+            }
 
 
-               }
-
-
-
-
-
-           }
-
-
-
-
-
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
 
--- ALG_HUGO_MAIA/Ficha_java/src/Ficha_ficheiros_extra/Exercicio3_funcoes.java
+++ ALG_HUGO_MAIA/Ficha_java/src/Ficha_ficheiros_extra/Exercicio3_funcoes.java
@@ -1,10 +1,7 @@
 package Ficha_ficheiros_extra;
 
 import javax.imageio.IIOException;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Exercicio3_funcoes {
@@ -21,14 +18,14 @@ public class Exercicio3_funcoes {
         Scanner input = new Scanner(System.in);
 
         File ficheiro = new File(caminho);
-        Scanner conteudo = new Scanner(ficheiro);
+
 
         while (validacao != true) {
             System.out.println("insira o seu user :");
             String user = input.nextLine();
             System.out.println("insira a sua password :");
             String password = input.nextLine();
-
+            Scanner conteudo = new Scanner(ficheiro);
             while (conteudo.hasNextLine()) {
 
                 String linha = conteudo.nextLine();
@@ -46,6 +43,7 @@ public class Exercicio3_funcoes {
     }
 
     /**
+     * funcao de gerir as bases de dados
      *
      * @throws IOException
      */
@@ -63,82 +61,446 @@ public class Exercicio3_funcoes {
 
             Scanner input = new Scanner(System.in);
             String opcao = input.nextLine().trim().toLowerCase();
-
+            String dados = "";
             switch (opcao) {
                 case "1":
+
                     System.out.println("1 -  Novo Cliente: " +
                             "\n2 - Atualizar Contactos do Cliente: ");
+
                     File ficheiro = new File("C:/Users/softdev/Desktop/cesae_02_2024/ALG_HUGO_MAIA/Ficha_java/src/Ficha_ficheiros_extra/Ex_03 Hotel Temático/clientesHotel.csv");
                     Scanner conteudo = new Scanner(ficheiro);
-                    String dados;
+                    int contador = 0;
+
                     opcao = input.nextLine().trim().toLowerCase();
 
-                            switch (opcao)
-                            {   case "1":
-                                while(conteudo.hasNextLine()){
-                                    String linha = conteudo.nextLine();
-                                    String [] linhas= linha.split(";");
-                                    dados = Integer.toString(Integer.parseInt(linhas[0])+1);}
-                            }
-                                System.out.println("nome :");
-                                dados =";"+input.nextLine();
-                                System.out.println("data nascimento :");
-                                dados =";"+input.nextLine();
-                                System.out.println("telefone :");
-                                dados =";"+input.nextLine();
-                                System.out.println("mail :");
-                                dados =";"+input.nextLine();
 
+                    switch (opcao) {
+                        case "1":
+                            // verifica qual é o ultimo indice
+                            System.out.println("ok");
+                            while (conteudo.hasNextLine()) {
+                                String linha = conteudo.nextLine();
 
-                                FileWriter escrita =  new FileWriter(ficheiro, true);
-                                escrita.write(dados);
-
-
-                                break;
-
-                                case "2":
-
-                                    break;
-
-                                default:
-                                    break;
-
-
-
-
+                                String[] linhas = linha.split(";");
+                                dados = linhas[0].split("-")[0] + "-" + Integer.toString(Integer.parseInt(linhas[0].split("-")[1]) + 1);
 
                             }
 
 
+                            System.out.println("nome :");
+                            dados += ";" + input.nextLine();
+                            System.out.println("data nascimento :");
+                            dados += ";" + input.nextLine();
+                            System.out.println("telefone :");
+                            dados += ";" + input.nextLine();
+                            System.out.println("mail :");
+                            dados += ";" + input.nextLine();
+                            System.out.println(dados);
+
+                            FileWriter escrita = new FileWriter(ficheiro, true);
+                            escrita.write(dados);
+                            escrita.close();
+                            run = false;
+
+                            break;
+
+                        case "2":
+
+                            System.out.println(" indique o id do cliente");
+                            String id = input.nextLine();
+                            System.out.println("Qual o campo deseja alterar : " +
+                                    "\n 1 - telemovel " +
+                                    "\n 2 - mail");
+                            opcao = input.nextLine();
+                            System.out.println("Indique o novo dado");
+                            dados = input.nextLine();
+
+                            /// contar o tamano do texto
+
+                            while (conteudo.hasNextLine()) {
+                                conteudo.nextLine();
+                                contador++;
+
+                            }
+
+                            // criar arry e passar o texto
+                            String[] array = new String[contador];
+
+                            File ficheiro_ = new File("C:/Users/softdev/Desktop/cesae_02_2024/ALG_HUGO_MAIA/Ficha_java/src/Ficha_ficheiros_extra/Ex_03 Hotel Temático/clientesHotel.csv");
+                            Scanner conteudo_ = new Scanner(ficheiro_);
+                            contador = 0;
+
+                            while (conteudo_.hasNextLine()) {
+                                array[contador] = conteudo_.nextLine().toString();
+                                contador++;
+                            }
+                            PrintWriter writer = new PrintWriter(ficheiro);
+
+                            /// verificar o campo e alterar o campo
+                            for (int n = 0; n < array.length; n++) {
+                                String[] linha = array[n].split(";");
+
+                                if (linha[0].equals(id)) {
+                                    if (opcao.equals("1")) {
+                                        linha[3] = dados;
+                                        String linhas = linha[0] + ";" + linha[01] + ";" + linha[2] + ";" + linha[3] + ";" + linha[4];
+
+                                        writer.println(linhas);
+
+                                    } else if (opcao.equals("2")) {
+                                        System.out.println("ok");
+                                        linha[4] = dados;
+                                        String linhas = linha[0] + ";" + linha[01] + ";" + linha[2] + ";" + linha[3] + ";" + linha[4];
+                                        writer.println(linhas);
+                                    }
+
+                                } else {
+                                    writer.println(array[n]);
+                                }
 
 
+                            }
+                            writer.close();
+                            run = false;
+                            break;
+
+                        default:
+                            System.out.println(" indique um id correcto");
+                            break;
+
+                    }
                     break;
-               /**
+
+
                 case "2":
-                    System.out.println("");
+
+                    System.out.println("Novo tema;");
+
+                    File ficheiro2 = new File("C:/Users/softdev/Desktop/cesae_02_2024/ALG_HUGO_MAIA/ficha_java/src/Ficha_ficheiros_extra/Ex_03 Hotel Temático/temasHotel.csv");
+                    Scanner conteudo_tema = new Scanner(ficheiro2);
+
+                    // verifica p ultimo indice
+                    while (conteudo_tema.hasNextLine()) {
+                        String linha = conteudo_tema.nextLine();
+                        String[] linhas = linha.split(";");
+                        dados = "\n" + linhas[0].split("-")[0] + "-" + Integer.toString(Integer.parseInt(linhas[0].split("-")[1]) + 1);
+
+                    }
+
+                    System.out.println("nome :");
+                    dados += ";" + input.nextLine();
+                    System.out.println("multiplicador de preço :");
+                    dados += ";" + input.nextLine();
+
+                    FileWriter escrita = new FileWriter(ficheiro2, true);
+                    escrita.write(dados);
+                    escrita.close();
+                    run = false;
                     break;
 
                 case "3":
 
+                    System.out.println("Novo Quarto :");
+
+                    File ficheiro3 = new File("C:\\Users\\softdev\\Desktop\\cesae_02_2024\\ALG_HUGO_MAIA\\Ficha_java\\src\\Ficha_ficheiros_extra\\Ex_03 Hotel Temático\\quartosHotel.csv");
+                    Scanner conteudo_quarto = new Scanner(ficheiro3);
+
+                    // verifica p ultimo indice
+                    while (conteudo_quarto.hasNextLine()) {
+                        String linha = conteudo_quarto.nextLine();
+                        String[] linhas = linha.split(";");
+                        dados = "\n" + linhas[0].split("-")[0] + "-" + Integer.toString(Integer.parseInt(linhas[0].split("-")[1]) + 1);
+
+                    }
+
+                    System.out.println("tema:");
+                    dados += ";" + input.nextLine();
+                    System.out.println("tipo de quarto :" +
+                            "\n Single, duplo ou suite");
+                    dados += ";" + input.nextLine().toUpperCase().trim();
+
+                    FileWriter escrita2 = new FileWriter(ficheiro3, true);
+                    escrita2.write(dados);
+                    escrita2.close();
+                    run = false;
                     break;
+
                 case "4":
+
+                    //---------------------------------
+
+
+                    System.out.println("1 -  Novas credencias: " +
+                            "\n2 - Eliminar credenciais : ");
+                    File ficheiro4 = new File("C:\\Users\\softdev\\Desktop\\cesae_02_2024\\ALG_HUGO_MAIA\\Ficha_java\\src\\Ficha_ficheiros_extra\\Ex_03 Hotel Temático\\loginHotel.txt");
+                    Scanner conteudo_credencias = new Scanner(ficheiro4);
+
+                    opcao = input.nextLine().trim().toLowerCase();
+
+
+                    switch (opcao) {
+                        case "1":
+                            // cria nova credencial
+                            while (conteudo_credencias.hasNextLine()) {
+                                String linha = conteudo_credencias.nextLine();
+                                String[] linhas = linha.split(";");
+                                dados = "\n" + linhas[0].split("-")[0] + "-" + Integer.toString(Integer.parseInt(linhas[0].split("-")[1]) + 1);
+                            }
+
+                            System.out.println("username :");
+                            dados += ";" + input.nextLine();
+                            System.out.println("password :");
+                            dados += ";" + input.nextLine();
+
+                            FileWriter escrita4 = new FileWriter(ficheiro4, true);
+                            escrita4.write(dados);
+                            escrita4.close();
+                            run = false;
+
+                            break;
+
+                        case "2":
+
+                            System.out.println(" indique username");
+                            String id = input.nextLine();
+                            contador = 0;
+
+
+                            while (conteudo_credencias.hasNextLine()) {
+                                conteudo_credencias.nextLine();
+                                contador++;
+                            }
+                            String[] array = new String[contador];
+                            File ficheiro5 = new File("C:\\Users\\softdev\\Desktop\\cesae_02_2024\\ALG_HUGO_MAIA\\Ficha_java\\src\\Ficha_ficheiros_extra\\Ex_03 Hotel Temático\\loginHotel.txt");
+                            Scanner conteudo_credencias_ = new Scanner(ficheiro5);
+                            for (String n : array) {
+                                n = conteudo_credencias_.nextLine();
+                            }
+
+
+                            run = false;
+                            break;
+
+                        default:
+                            System.out.println(" indique um id correcto");
+                            break;
+
+
+                    }
 
                     break;
 
                 case "5":
 
+                    System.out.println("Novo produto;");
+
+                    File ficheiro5 = new File("C:\\Users\\softdev\\Desktop\\cesae_02_2024\\ALG_HUGO_MAIA\\Ficha_java\\src\\Ficha_ficheiros_extra\\Ex_03 Hotel Temático\\produtosHotel.csv");
+                    Scanner conteudo_produto = new Scanner(ficheiro5);
+
+                    // verifica p ultimo indice
+                    while (conteudo_produto.hasNextLine()) {
+                        String linha = conteudo_produto.nextLine();
+                        String[] linhas = linha.split(";");
+                        dados = linhas[0].split("-")[0] + "-" + Integer.toString(Integer.parseInt(linhas[0].split("-")[1]) + 1);
+
+                    }
+
+                    System.out.println("nome :");
+                    dados += ";" + input.nextLine();
+                    System.out.println("preço :");
+                    dados += ";" + input.nextLine();
+
+                    FileWriter escrita3 = new FileWriter(ficheiro5, true);
+                    escrita3.write("\n" + dados);
+                    escrita3.close();
+                    run = false;
                     break;
+
 
                 default:
 
                     System.out.println("Escolha uma opcao valida :");
                     break;
-                */
+
 
             }
 
 
         }
+
+
+    }
+
+    /**
+     * funcao consultar clientes
+     *
+     * @return
+     * @throws FileNotFoundException
+     */
+    public static String consultarclientes() throws FileNotFoundException {
+
+        Scanner input = new Scanner(System.in);
+        String dados = "";
+        System.out.println(" Consultar clientes" +
+                "\n1 -  idCliente: " +
+                "\n2 - telemovel: " +
+                "\n3 - por email");
+        int contador = 0;
+        String opcao = input.nextLine().trim().toLowerCase();
+        File ficheiro = new File("C:/Users/softdev/Desktop/cesae_02_2024/ALG_HUGO_MAIA/Ficha_java/src/Ficha_ficheiros_extra/Ex_03 Hotel Temático/clientesHotel.csv");
+        Scanner conteudo = new Scanner(ficheiro);
+
+        switch (opcao) {
+            case "1":
+                System.out.println("indique o id");
+                dados = input.nextLine();
+                while (conteudo.hasNextLine()) {
+                    String linha = conteudo.nextLine();
+                    String[] linhas = linha.split(";");
+                    if (linhas[0].equals(dados)) {
+                        return "" + linhas[1] + " - " + linhas[2] + " - " + linhas[3] + " - " + linhas[4];
+                    }
+                }
+                break;
+
+            case "2":
+
+                System.out.println("indique o telemovel");
+                dados = input.nextLine();
+                while (conteudo.hasNextLine()) {
+                    String linha = conteudo.nextLine();
+                    String[] linhas = linha.split(";");
+                    if (linhas[3].equals(dados)) {
+                        dados = "" + linhas[1] + " - " + linhas[2] + " - " + linhas[3] + " - " + linhas[4];
+                    }
+                }
+                break;
+
+            case "3":
+
+                System.out.println("indique o email");
+                dados = input.nextLine();
+                while (conteudo.hasNextLine()) {
+                    String linha = conteudo.nextLine();
+                    String[] linhas = linha.split(";");
+                    if (linhas[4].equals(dados)) {
+                        dados = "" + linhas[1] + " - " + linhas[2] + " - " + linhas[3] + " - " + linhas[4];
+                    }
+                }
+                break;
+            default:
+
+                dados = "valores invalidos";
+        }
+        return dados;
+    }
+    //-----------------------------------------------------
+
+public static void consultar_quartos() throws FileNotFoundException{
+
+
+    Scanner input = new Scanner(System.in);
+    String dados = "";
+
+    System.out.println(" Consultar quartos" +
+            "\n1 -  Quartos disponivel: " +
+            "\n2 - Quarto disponivel por tema : " +
+            "\n3 - Quarto disponivel por tipo");
+
+
+    int contador = 0;
+    String opcao = input.nextLine().trim().toLowerCase();
+    File ficheiro = new File("C:\\Users\\softdev\\Desktop\\cesae_02_2024\\ALG_HUGO_MAIA\\Ficha_java\\src\\Ficha_ficheiros_extra\\Ex_03 Hotel Temático\\quartosHotel.csv");
+    Scanner conteudo_quartos = new Scanner(ficheiro);
+
+
+
+
+    switch (opcao) {
+        case "1":
+
+            System.out.println("Consultar, quartos");
+            dados = input.nextLine();
+            while (conteudo_quartos.hasNextLine()) {
+
+                File ficheiro2 = new File("C:\\Users\\softdev\\Desktop\\cesae_02_2024\\ALG_HUGO_MAIA\\Ficha_java\\src\\Ficha_ficheiros_extra\\Ex_03 Hotel Temático\\reservasHotel.csv");
+                Scanner conteudo_reservas = new Scanner(ficheiro2);
+
+                String linha = conteudo_quartos.nextLine();
+                String linhas [] = linha.split(";"); /// coluna 0 da o id do quarto
+                String quarto = linhas[0];
+
+                while (conteudo_reservas.hasNextLine())
+                {
+
+                    String linha_reserva = conteudo_reservas.nextLine();
+                    String linhas_reservas [] = linha_reserva.split(";"); /// coluna 0 da o id do quarto
+                    String quarto_reservas = linhas_reservas[3];
+
+
+
+                }
+
+                String[] linhas = linha.split(";");
+
+                if (linhas[0].equals(dados) ) {
+                    dados = "" + linhas[1] + " - " + linhas[2] + " - " + linhas[3] + " - " + linhas[4]+"\n";
+                }
+            }
+
+
+
+
+
+
+
+            break;
+
+        case "2":
+
+            System.out.println("indique o telemovel");
+            dados = input.nextLine();
+            while (conteudo.hasNextLine()) {
+                String linha = conteudo.nextLine();
+                String[] linhas = linha.split(";"); // compara o telefone com os dados
+                if (linhas[3].equals(dados)) {
+                    dados = "" + linhas[1] + " - " + linhas[2] + " - " + linhas[3] + " - " + linhas[4];
+                }
+            }
+            break;
+
+        case "3":
+
+            System.out.println("indique o email");
+            dados = input.nextLine();
+            while (conteudo.hasNextLine()) {
+                String linha = conteudo.nextLine();
+                String[] linhas = linha.split(";");
+                if (linhas[4].equals(dados)) {
+                    dados = "" + linhas[1] + " - " + linhas[2] + " - " + linhas[3] + " - " + linhas[4];
+                }
+            }
+            break;
+        default:
+
+            dados = "valores invalidos";
+    }
+    return dados;
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
 
 
 
