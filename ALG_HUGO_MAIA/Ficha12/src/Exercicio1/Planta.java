d959abb60d203441ce07743fa01208780fbba2ff
Hugo Maia <130175150+hugotwin@users.noreply.github.com>
2024-04-16 16:55:35 +0100

    16_04_2023_2

---
+++ ALG_HUGO_MAIA/Ficha12/src/Exercicio1/Planta.java
@@ -0,0 +1,43 @@
+package Exercicio1;
+
+import Exercicio1.Enum.TipoFamilia;
+
+public class Planta extends SerVivo{
+
+    private TipoFamilia familia;
+    private  int  grau_defesa;
+
+
+    public Planta(String nome, String especie, String pais_origem, int idade, TipoFamilia familia, int grau_defesa) {
+        super(nome, especie, pais_origem, idade);
+
+        this.familia=familia;
+        this.inserirdefesa(grau_defesa);
+
+
+
+
+    }
+    public void inserirdefesa(int grau_defesa)
+    {
+        if(grau_defesa<0 || grau_defesa>6)
+        {
+            System.out.println("valor invalido");
+        }else{
+
+            this.grau_defesa=grau_defesa;
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ ALG_HUGO_MAIA/Ficha12/src/Main.java
@@ -0,0 +1,8 @@
+import Exercicio1.Animal;
+import Exercicio1.Enum.TipoDieta;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal dumbo = new Animal("Dumbo","Elefante","Quenia",40,1200,50, TipoDieta.HERVIBORO,"IIIIIII");
+    }
+}
