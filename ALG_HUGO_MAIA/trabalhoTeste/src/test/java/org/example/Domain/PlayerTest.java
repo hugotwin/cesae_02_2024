1648cad096d508a887db03d608538b2319e5744d
Hugo Maia <130175150+hugotwin@users.noreply.github.com>
2024-06-04 08:51:13 +0100

    04/06/2023

---
+++ ALG_HUGO_MAIA/trabalhoTeste/src/test/java/org/example/Domain/PlayerTest.java
@@ -0,0 +1,49 @@
+package org.example.Domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+
+        player = new Player();
+
+
+
+    }
+
+    @Test
+    void getNumber() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void getNationality() {
+    }
+
+    @Test
+    void getDefenceRanking() {
+    }
+
+    @Test
+    void getAttackRanking() {
+    }
+
+    @Test
+    void getValue() {
+    }
+}
